475aada0082bf06e90507e9038574583d9c88ebc
Kushalchavan <141314547+Kushalchavan@users.noreply.github.com>
2023-10-22 22:36:38 +0530

    main

---
+++ Color_toggler/app.js
@@ -0,0 +1,10 @@
+let toggler = document.getElementById('switch');
+
+toggler.addEventListener('click', () => {
+    if(toggler.checked === true){
+        document.body.style.backgroundColor = '#000';
+    }
+    else{
+        document.body.style.backgroundColor = '#fff';
+    }
+})
